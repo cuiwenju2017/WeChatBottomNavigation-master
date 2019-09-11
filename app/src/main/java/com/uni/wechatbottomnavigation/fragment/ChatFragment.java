ad68484bad8ed672a165c42ee224b1b8956d418e
崔文举 <1755140651@qq.com>
2019-09-11 15:15:12 +0800

    2019/9/11

--- app/src/main/java/com/uni/wechatbottomnavigation/fragment/ChatFragment.java
+++ app/src/main/java/com/uni/wechatbottomnavigation/fragment/ChatFragment.java
@@ -7,23 +7,43 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.uni.wechatbottomnavigation.R;
+import com.uni.wechatbottomnavigation.util.L;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * 微信
  */
 public class ChatFragment extends Fragment {
 
+    @BindView(R.id.tv)
+    TextView tv;
+    Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 
+    @OnClick(R.id.tv)
+    public void onViewClicked() {
+        Toast.makeText(getContext(), "点击了我", Toast.LENGTH_SHORT).show();
+        L.d("点击了我");
     }
 }
